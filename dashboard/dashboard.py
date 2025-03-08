c0c6073150b335a2ad3d1c28eb8d0e81fcd3057e
Robby Saidi <robbysaidiii@gmail.com>
2025-03-08 08:51:32 +0700

    Update requirements.txt to fix dependency issues

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 
 st.title('Dashboard Latihan Machine Learning_mc211d5y2136')
 

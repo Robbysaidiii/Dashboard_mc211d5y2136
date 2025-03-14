f5aa14dfb884cef45d8d3a953f6c12c508466e23
Robby Saidi Prasetyo <157108492+Robbysaidiii@users.noreply.github.com>
2025-03-14 18:05:10 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -53,7 +53,7 @@ else:
     if option == "Distribusi Jumlah Penyewaan Sepeda":
         st.subheader("📈 Distribusi Jumlah Penyewaan Sepeda")
         fig, ax = plt.subplots()
-        sns.histplot(filtered_df['cnt_y'], bins=30, kde=True, ax=ax)
+        sns.histplot(filtered_df['cnt_y'],(day['cnt'], bins=30, kde=True)
         ax.set_title("Data Penyewaan Sepeda")
         ax.set_xlabel("Jumlah Sewa Sepeda")
         ax.set_ylabel("Frekuensi")
